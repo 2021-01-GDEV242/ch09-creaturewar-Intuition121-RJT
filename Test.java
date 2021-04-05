a66cbc00b45514ebac6a596ada9ccaffd981df7e
Intuition121-RJT <77683482+Intuition121-RJT@users.noreply.github.com>
2021-04-05 18:13:06 -0400

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,29 @@
+
+/**
+ * Write a description of class Test here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Test
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Test
+     */
+    public Test()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * Test the battle
+     */
+    public void battleTest()
+    {
+        new Battle();
+    }
+}
